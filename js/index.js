190d5c7e224710764a964f4ca47b1d489046f4ec
Crystal Ogbuachi <cogbuachi2@gmail.com>
2025-05-18 18:06:02 -0400

    responsiveness added

---
+++ js/index.js
@@ -0,0 +1,5 @@
+var body = document.getElementsByTagName("body")[0]
+console.log(body);
+const footer = document.createElement("footer")
+footer.innerHTML = "Crystal Ogbuachi footer"
+body.appendChild(footer)
